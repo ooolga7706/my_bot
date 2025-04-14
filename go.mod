2072b2b250fb7f17898a372bd547970712bc65f6
Oemi <oemi.ua@gmail.com>
2025-04-14 21:17:08 +0200

    Первый коммит

---
+++ go.mod
@@ -0,0 +1,8 @@
+module mybot
+
+go 1.24.2
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible // indirect
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+    "log"
+    "strings"
+
+    tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+func main() {
+    bot, err := tgbotapi.NewBotAPI("7634588357:AAGz3Nfh6ET2bB-sdoTLUTN6qZVVTHYqvbw")
+    if err != nil {
+        log.Panic("Ошибка создания бота:", err)
+    }
+
+    bot.Debug = true
+
+    u := tgbotapi.NewUpdate(0)
+    u.Timeout = 60
+
+    updates, err := bot.GetUpdatesChan(u)
+    if err != nil {
+        log.Panic("Ошибка получения обновлений:", err)
+    }
+
+    for update := range updates {
+        if update.Message == nil {
+            continue
+        }
+
+        log.Printf("[%s | %d] %s", update.Message.From.UserName, update.Message.Chat.ID, update.Message.Text)
+
+        if strings.ToLower(update.Message.Text) == "/start" {
+            msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Привет! Я простой Go-бот.")
+            _, err := bot.Send(msg)
+            if err != nil {
+                log.Println("Ошибка при отправке:", err)
+            }
+        }
+    }
+}
